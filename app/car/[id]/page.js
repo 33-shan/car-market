a9262feb3302195446e7bf22432e18905a7f5cc1
Chen Hui-Shan <a0935110667@gmail.com>
2025-06-17 22:12:40 +0800

    更新網站內容

--- app/car/[id]/page.js
+++ app/car/[id]/page.js
@@ -1,6 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
+import Image from "next/image"; 
 
 export default function CarDetailPage() {
   const { id } = useParams(); // 拿到網址中的 id
