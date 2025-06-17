8880fe5c199871c110d20b1b7a6a98548ea2a098
Chen Hui-Shan <a0935110667@gmail.com>
2025-06-17 15:14:47 +0800

    🧪 測試首頁是否顯示成功

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <main className="p-10">
       <h1 className="text-3xl font-bold">✅ 成功首頁</h1>
-      <p>如果你看到這個，代表 `app/page.js` 已經正確部署。</p>
+      <p className="text-red-500">如果你看到這個，代表 `app/page.js` 已經正確部署。</p>
     </main>
   );
 }
